aa1b9c76403c221d2b8b825aaede344902d7968e
Morten Albring <mortenalbring@gmail.com>
2020-09-14 11:39:16 +0100

    Initial getting of podcast duration for 99pi

--- PodcastStats/Program.cs
+++ PodcastStats/Program.cs
@@ -1,9 +1,45 @@
-﻿namespace PodcastStats
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Syndication;
+using System.Xml;
+
+
+namespace PodcastStats
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            
+            string url = "https://feeds.99percentinvisible.org/";
+            XmlReader reader = XmlReader.Create(url);
+            SyndicationFeed feed = SyndicationFeed.Load(reader);
+            reader.Close();
+            Console.WriteLine(feed.Items.Count());
+            var output = new Dictionary<DateTimeOffset, string>();
+            
+            foreach (SyndicationItem item in feed.Items)
+            {
+                var publishDate = item.PublishDate;
+                var durationStr = "";
+                
+                var durationEl = item.ElementExtensions.ReadElementExtensions<XmlElement>("duration", "http://www.itunes.com/dtds/podcast-1.0.dtd")[0];
+
+                if (durationEl != null)
+                {
+                    durationStr = durationEl.InnerText;
+                    
+                    output.Add(item.PublishDate, durationStr);
+                }
+                String subject = item.Title.Text;    
+                String summary = item.Summary.Text;
+            }
+
+            foreach (var d in output)
+            {
+                Console.WriteLine(d.Key.Date + ","  + d.Value);
+            }
         }
     }
 }
